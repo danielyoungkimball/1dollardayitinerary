678dcd05e79f909420341ace479bd10b365491fe
Daniel Kimball <danielyoungkimball@outlook.com>
2025-07-04 15:17:06 -0700

    fix turbo and debug log

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,5 +1,13 @@
 import type { NextConfig } from "next";
 
+import fs from "fs";
+
+// debugging vercel build
+if (!fs.existsSync("node_modules/@types/react")) {
+  console.warn("WARNING: @types/react not found in web build!");
+}
+
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
