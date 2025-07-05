9c0bb2cc6e9c3b2ef5ba3709f9a4b4cd5fadb4c7
Daniel Kimball <danielyoungkimball@outlook.com>
2025-07-04 19:52:39 -0700

    fix

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,13 +1,5 @@
 import type { NextConfig } from "next";
 
-import fs from "fs";
-
-// debugging vercel build
-if (!fs.existsSync("node_modules/@types/react")) {
-  console.warn("WARNING: @types/react not found in web build!");
-}
-
-
 const nextConfig: NextConfig = {
   /* config options here */
 };
